2c52406c18fe1c1d93ebb37c5e7499466ef49f82
shbabu187 <shyam.tict.sb@gmail.com>
2023-11-02 18:47:21 +0530

    setup to run hello

---
+++ src/main/java/learn/demo/DemoController.java
@@ -0,0 +1,12 @@
+package learn.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+    @GetMapping("/hello")
+    public String Hello() {
+        return "Hello There !!";
+    }
+}
