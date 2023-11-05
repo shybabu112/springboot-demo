a813a53e74676fa24c64888cb800d6d94bdd806d
shbabu187 <shyam.tict.sb@gmail.com>
2023-11-05 12:21:52 +0530

    return message from properties

--- src/main/java/learn/demo/DemoController.java
+++ src/main/java/learn/demo/DemoController.java
@@ -1,12 +1,15 @@
 package learn.demo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoController {
+    @Value("${api.message:Hello exception}")
+    private String message;
     @GetMapping("/hello")
     public String Hello() {
-        return "Hello There !!";
+        return message;
     }
 }
